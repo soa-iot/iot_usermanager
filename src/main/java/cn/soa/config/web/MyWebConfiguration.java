158ae1fe3b7e2023a0b355775f9ba2c4af1dc11f
zhu-gang <632452599@qq.com>
2019-04-25 10:12:00 +0800

    2019.4.25 解决跨域cookie保存错误

--- src/main/java/cn/soa/config/web/MyWebConfiguration.java
+++ src/main/java/cn/soa/config/web/MyWebConfiguration.java
@@ -1,24 +1,30 @@
 package cn.soa.config.web;
 
+import javax.servlet.ServletRequest;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
-public class MyWebConfiguration extends WebMvcConfigurationSupport {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-    
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        super.addResourceHandlers(registry);
-    }
-
-
-}
+//@Configuration
+//public class MyWebConfiguration extends WebMvcConfigurationSupport {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//        		.allowedOrigins("*")
+//        		.allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+//        		.maxAge(3600)
+//        		.allowCredentials(true);
+//    }
+//    
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//        super.addResourceHandlers(registry);
+//    }
+//
+//
+//}
