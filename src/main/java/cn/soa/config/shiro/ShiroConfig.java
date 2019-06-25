2b96622c6b0591aeda77f93fce8ebd621802cb65
zhu-gang <632452599@qq.com>
2019-06-25 12:32:06 +0800

    1

--- src/main/java/cn/soa/config/shiro/ShiroConfig.java
+++ src/main/java/cn/soa/config/shiro/ShiroConfig.java
@@ -36,55 +36,63 @@ import java.util.Map;
 @Configuration
 public class ShiroConfig {	
 
-	 @Bean
-	    public SecurityManager securityManager() {
-	        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-	        // 设置realm.
-	        securityManager.setRealm(myShiroRealm());
-	        securityManager.setCacheManager(new MemoryConstrainedCacheManager());
-	        securityManager.setSessionManager(sessionManager());
-	        // 自定义缓存实现 使用redis
-	        //securityManager.setCacheManager(new MemoryConstrainedCacheManager());
-	        // 自定义session管理 使用redis
-	        //securityManager.setSessionManager(sessionManager());
-	        return securityManager;
-	    }
+	/**   
+	 * @Title: securityManager   
+	 * @Description:  初始化安全管理 
+	 * @return: SecurityManager        
+	 */  
+	@Bean
+	public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 设置realm
+        securityManager.setRealm(myShiroRealm());
+        securityManager.setCacheManager(new MemoryConstrainedCacheManager());
+        securityManager.setSessionManager(sessionManager());
+        // 自定义缓存实现 使用redis
+        //securityManager.setCacheManager(redisManager());
+        // 自定义session管理 使用redis
+        //securityManager.setSessionManager(redisSessionDAO());
+        return securityManager;
+    }
     
-     /**   
-      * @Title: shiroFilter   
-      * @Description: shiro的拦截器       
-      * @return: ShiroFilterFactoryBean        
-      */  
+	/**   
+	 * @Title: shiroFilter   
+	 * @Description: shiro的拦截器       
+	 * @return: ShiroFilterFactoryBean        
+  	 */  
     @Bean
     public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        shiroFilterFactoryBean.setSecurityManager(securityManager);
-        //如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
-        shiroFilterFactoryBean.setLoginUrl("/html/userCenter/login.html");
-        // 登录成功后要跳转的链接
-        shiroFilterFactoryBean.setSuccessUrl("/html/userCenter/index.html");
-        
-        //自定义拦截器
-        //Map<String, Filter> filtersMap = new LinkedHashMap<String, Filter>();
-        //限制同一帐号同时在线的个数。
-        //filtersMap.put("kickout", kickoutSessionControlFilter());
-        //shiroFilterFactoryBean.setFilters(filtersMap);
-        
-        /*
-         *  权限控制设置map
-         */
-        // 拦截器.
-        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
-        // 配置不会被拦截的链接 顺序判断
-        //filterChainDefinitionMap.put("/html/**/*.html", "anon");
-        //filterChainDefinitionMap.put("/user/*/*", "anon");
+    	 ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+ 	    shiroFilterFactoryBean.setSecurityManager(securityManager);
+ 	    //如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+ 	    shiroFilterFactoryBean.setLoginUrl("/html/userCenter/login.html");
+ 	    // 登录成功后要跳转的链接
+ 	    shiroFilterFactoryBean.setSuccessUrl("/html/userCenter/index.html");
+ 	    
+ 	    //自定义拦截器
+ 	    //Map<String, Filter> filtersMap = new LinkedHashMap<String, Filter>();
+ 	    //限制同一帐号同时在线的个数。
+ 	    //filtersMap.put("kickout", kickoutSessionControlFilter());
+ 	    //shiroFilterFactoryBean.setFilters(filtersMap);
+ 	    
+ 	    /*
+ 	     *  权限控制设置map
+ 	     */
+ 	    // 拦截器.
+ 	    Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+ 	    // 配置不会被拦截的链接 顺序判断
+ 	    //filterChainDefinitionMap.put("/html/**/*.html", "anon");
+ 	    //filterChainDefinitionMap.put("/user/*/*", "anon");
+ 	
+ 	    //配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
+ 	    //filterChainDefinitionMap.put("/user/logout", "logout");
+ 	
+ 	    // 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 :这是一个坑呢，一不小心代码就不好使了;
+ 	    // ① authc:所有url都必须认证通过才可以访问; ② anon:所有url都都可以匿名访问
+ 	    filterChainDefinitionMap.put("/**", "anon");
 
         //配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
         //filterChainDefinitionMap.put("/user/logout", "logout");
-
-        // 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 :这是一个坑呢，一不小心代码就不好使了;
-        // ① authc:所有url都必须认证通过才可以访问; ② anon:所有url都都可以匿名访问
-        filterChainDefinitionMap.put("/**", "anon");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return shiroFilterFactoryBean;
     }
